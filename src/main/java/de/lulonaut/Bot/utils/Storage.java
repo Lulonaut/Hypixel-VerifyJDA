bd4cb6f37ecf9023db84073068b36035f7e264fc
Lulonaut <lars.richter.2006@gmail.com>
2020-11-28 13:33:40 +0100

    work on Storage.java

--- src/main/java/de/lulonaut/Bot/utils/Storage.java
+++ src/main/java/de/lulonaut/Bot/utils/Storage.java
@@ -7,19 +7,21 @@ import org.json.simple.parser.ParseException;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import static java.lang.Integer.parseInt;
 
 class Storage {
 
     public static void main(String[] args) throws IOException, ParseException {
-        /*JSONObject object = new JSONObject();
-        object.put("name", "test");
-        object.put("test", true);
 
-        PrintWriter printWriter = new PrintWriter("test.json");
-        printWriter.write(object.toString());
-        printWriter.flush();
-        printWriter.close();*/
-        readJSON();
+        messagesJSON("thiswontwork", "1234657", true);
+        messagesJSON("thiswontwork", "1234657", true);
+        messagesJSON("thiswontwork", "1234657", true);
+        messagesJSON("thiswontwork", "1234657", true);
+        messagesJSON("thiswontwork", "1234657", true);
+        messagesJSON("thiswontwork", "1234657", true);
+
     }
 
     public static void readJSON() {
@@ -44,6 +46,54 @@ class Storage {
         } catch (ParseException | IOException e) {
             e.printStackTrace();
         }
+    }
 
+    public static void messagesJSON(String GuildID, String UserID, boolean addOrRemove) throws IOException, ParseException {
+        //read current state
+        JSONObject object = new JSONObject();
+        FileReader reader = new FileReader("test.json");
+
+        JSONObject jsonObject = (JSONObject) new JSONParser().parse(reader);
+        reader.close();
+        try {
+            JSONObject messages = (JSONObject) jsonObject.get("messages");
+            JSONObject guildMessages = (JSONObject) messages.get(GuildID);
+            if (guildMessages == null) {
+                JSONObject newGuild = new JSONObject();
+                newGuild.put(UserID, 0);
+                messages.put(GuildID, newGuild);
+                System.out.println(messages);
+
+                PrintWriter printWriter = new PrintWriter("test.json");
+                printWriter.write(String.valueOf(jsonObject));
+                printWriter.flush();
+                printWriter.close();
+
+            } else {
+                System.out.println("else");
+                try {
+                    String current = String.valueOf(guildMessages.get(UserID));
+                } catch (NullPointerException e) {
+                    System.out.println("null pointer, setting to 0");
+                    guildMessages.put(UserID, 0);
+                }
+
+                String current = String.valueOf(guildMessages.get(UserID));
+                int currenntmsg = parseInt(current);
+                currenntmsg = currenntmsg + 1;
+                System.out.println("current:" + currenntmsg);
+
+                guildMessages.put(UserID, currenntmsg);
+                PrintWriter printWriter = new PrintWriter("test.json");
+                printWriter.write(String.valueOf(jsonObject));
+                printWriter.flush();
+                printWriter.close();
+            }
+
+            System.out.println(guildMessages);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
