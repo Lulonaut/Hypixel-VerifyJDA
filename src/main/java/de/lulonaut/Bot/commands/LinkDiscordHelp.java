a60e7095718688e77cfa48753f805ef38ce5dab9
Lulonaut <lars.richter.2006@gmail.com>
2020-12-02 20:23:22 +0100

    Format String

--- src/main/java/de/lulonaut/Bot/commands/LinkDiscordHelp.java
+++ src/main/java/de/lulonaut/Bot/commands/LinkDiscordHelp.java
@@ -9,12 +9,13 @@ public class LinkDiscordHelp extends ListenerAdapter {
     public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event) {
         String[] msg = event.getMessage().getContentRaw().split(" ");
 
-        if(msg[0].equalsIgnoreCase(Main.PREFIX + "linkdc")){
-            event.getChannel().sendMessage("1.Go to /lobby on Hypixel\n" +
-                    "2.Click on your Head (second Slot)\n" +
-                    "3.Click on the twitter symbol (column 3, row 4)\n" +
-                    "4.Click on the Discord Symbol (second to last one) and paste your Discord Link in the Chat" +
-                    "\nhttps://gfycat.com/dentaltemptingleonberger (Stolen video)").queue();
+        if (msg[0].equalsIgnoreCase(Main.PREFIX + "linkdc")) {
+            event.getChannel().sendMessage(
+                    "1.Go to /lobby on Hypixel\n" +
+                            "2.Click on your Head (second Slot)\n" +
+                            "3.Click on the twitter symbol (column 3, row 4)\n" +
+                            "4.Click on the Discord Symbol (second to last one) and paste your Discord Link in the Chat\n" +
+                            "https://gfycat.com/dentaltemptingleonberger (Stolen video)").queue();
         }
     }
 }
