f59e803e18b19afec11a80477c3da88f1644b22a
Lulonaut <lars.richter.2006@gmail.com>
2020-12-02 20:23:38 +0100

    Update Test

--- src/main/java/de/lulonaut/Bot/utils/ConfigTest.java
+++ src/main/java/de/lulonaut/Bot/utils/ConfigTest.java
@@ -1,11 +1,12 @@
 package de.lulonaut.Bot.utils;
 
 import de.lulonaut.Bot.errors.ConfigException;
+import de.lulonaut.Bot.errors.ConfigNotFoundException;
 
 import java.io.IOException;
 
 public class ConfigTest {
-    public static void main(String[] args) throws IOException, ConfigException {
+    public static void main(String[] args) throws IOException, ConfigException, ConfigNotFoundException {
 
         System.out.println(Config.getConf("prefix", false));
 
