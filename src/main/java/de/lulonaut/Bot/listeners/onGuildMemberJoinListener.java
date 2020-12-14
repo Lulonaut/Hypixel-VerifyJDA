a405f2c6e076639914541c32e4f3d5649eb1fa5d
Lulonaut <lars.richter.2006@gmail.com>
2020-12-14 20:46:20 +0100

    add file

---
+++ src/main/java/de/lulonaut/Bot/listeners/onGuildMemberJoinListener.java
@@ -0,0 +1,11 @@
+package de.lulonaut.Bot.listeners;
+
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class onGuildMemberJoinListener extends ListenerAdapter {
+
+    public void onGuildMemberJoin(GuildMemberJoinEvent event) {
+        //TODO aber nicht heute :)
+    }
+}
