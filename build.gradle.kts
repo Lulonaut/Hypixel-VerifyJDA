1fbdad217388cab5945078b2df607c202409a17c
Lulonaut <lars.richter.2006@gmail.com>
2021-01-19 00:03:40 +0100

    update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,18 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     java
-    `maven-publish`
     kotlin("jvm") version "1.4.30-M1"
+    id("com.github.johnrengelman.shadow") version "4.0.4"
 }
+
+
 
 repositories {
     mavenLocal()
+    mavenCentral()
+    jcenter()
     maven {
         url = uri("https://jitpack.io")
     }
@@ -20,7 +25,6 @@ repositories {
         url = uri("https://repo.maven.apache.org/maven2/")
     }
     maven("https://dl.bintray.com/kotlin/kotlin-eap")
-    mavenCentral()
 }
 
 dependencies {
@@ -29,7 +33,6 @@ dependencies {
     implementation("org.slf4j:slf4j-simple:1.7.12")
     implementation("org.json:json:20180130")
     implementation("com.github.ReflxctionDev:SimpleHypixelAPI:8eff50e")
-    implementation("com.googlecode.json-simple:json-simple:1.1.1")
     implementation("org.apache.commons:commons-lang3:3.11")
     implementation("redis.clients:jedis:3.4.1")
     implementation("com.github.Lulonaut:HypixelAPIWrapper:d7e93b2f9c")
@@ -41,11 +44,7 @@ version = "0.123"
 description = "JDABot"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
-publishing {
-    publications.create<MavenPublication>("maven") {
-        from(components["java"])
-    }
-}
+
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
     jvmTarget = "1.8"
@@ -54,16 +53,17 @@ val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.kotlinOptions {
     jvmTarget = "1.8"
 }
-
-tasks.withType<Jar> {
-    manifest {
-        attributes["Main-Class"] = "de.lulonaut.bot.Main"
+tasks {
+    named<ShadowJar>("shadowJar") {
+        archiveBaseName.set("shadow")
+        mergeServiceFiles()
+        manifest {
+            attributes(mapOf("Main-Class" to "de.lulonaut.bot.Main"))
+        }
     }
+}
 
-    from(sourceSets.main.get().output)
+tasks {
+    build { dependsOn(shadowJar) }
 
-    dependsOn(configurations.runtimeClasspath)
-    from({
-        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
-    })
 }
